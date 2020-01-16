1f2e9e86ad2d03c82181d169e1c7e6641b665ab3
TheInfiniteCode001 <47271339+TheInfiniteCode001@users.noreply.github.com>
2020-01-16 11:20:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
+import py_outdated.library.unused_stuffs.*
 
+a = UnusedShit(3)
+
+class Nothing(py_code.__skid__):
+  def __init__(self, outdated_code):
+    self.random_stuffs = list(outdated_code)
